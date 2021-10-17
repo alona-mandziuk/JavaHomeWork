7742dcb58bdf891de32bc41120a70e8ad46c8d3c
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2021-10-17 14:52:14 +0300

    init commit

--- src/com/pb/mandziuk/hw2/Calculator.java
+++ src/com/pb/mandziuk/hw2/Calculator.java
@@ -1,41 +1,49 @@
 package com.pb.mandziuk.hw2;
 
-import java.io.PrintStream;
+
 import java.util.Scanner;
 
 public class Calculator {
+
     public static void main(String[] args) {
         System.out.println("Try to do the math");
         Scanner scan = new Scanner(System.in);
         Double x;
         Double y;
-        double z;
+        Double z;
         char sign;
         System.out.println("Enter two numbers:");
         x = scan.nextDouble();
         y = scan.nextDouble();
         System.out.println("Enter what u wanna do with them: (+,-,*,/");
         sign = scan.next().charAt(0);
-switch(sign) {
-    case '+':
-        z = x + y;
-        break;
-    case '-':
-        z = x - y;
-        break;
-    case '*':
-        z = x * y;
-        break;
-    case '/':
-        if (y == 0) {
-            System.out.println("dont do this!!!");}
-        z = x / y;
-        break;
+        if (y == 0 && sign == '/') {
+            System.out.println("You've chased the wrong action");
+        }
 
-    default:
-        System.out.println("You've chased the wrong action");
-        return;}
-        System.out.println("The result is");
-            System.out.println(x + " " + sign + " " + y + " = " + z);}
-}
+                switch (sign) {
+                    case '+':
+                        z = x + y;
+                        break;
+                    case '-':
+                        z = x - y;
+                        break;
+                    case '*':
+                        z = x * y;
+                        break;
+
+                    case '/':
+                        if (y == 0) throw new ArithmeticException("you cannot divide to 0");
+                        z = x / y;
+                        break;
+
+                    default:
+                        System.out.println("You've chased the wrong action");
+                        return;
+                }
+            System.out.println("The result is");
+            System.out.println(x + " " + sign + " " + y + " = " + z);
+        }
+    }
+
 
