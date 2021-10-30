fb8ebffda9b90f5302293ec149856e9e4c00ac2d
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2021-10-30 17:59:14 +0300

    init commit

---
+++ src/com/pb/mandziuk/hw4/Anagram.java
@@ -0,0 +1,45 @@
+package com.pb.mandziuk.hw4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введіть першу пропозицію: ");
+        String first = in.nextLine().toUpperCase();
+        System.out.print("Введіть іншу: ");
+        String second = in.nextLine().toUpperCase();
+        String result = isAnagram(first, second);
+        System.out.println(result);
+    }
+
+    private static String isAnagram(String first, String second) {
+        if (first == null || second == null || first.equals("") || second.equals("")) {
+            return "No anagram.";
+        }
+        String answer = "";
+
+        String st = first.replaceAll("[,. ]+", "");
+        String nd = second.replaceAll("[,. ]+", "");
+
+        if (st.equals("") || nd.equals("")) {
+            return "No anagram.";
+        }
+
+        char[] arraySt = st.toCharArray();
+        char[] arrayNd = nd.toCharArray();
+        Arrays.sort(arraySt);
+        Arrays.sort(arrayNd);
+
+        if (Arrays.equals(arraySt, arrayNd)) {
+            answer = "Anagram.";
+        } else {
+            answer = "No anagram.";
+        }
+        return answer;
+    }
+
+
+}
